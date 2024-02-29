97dd2758c4d6a75135628fe84c4f276817f6a687
王亚 <1264178545@qq.com>
2024-02-29 16:00:58 +0800

    props

---
+++ src/views/props/Child.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="child">
+    我是子元素
+    <h6>玩具：{{ toy }}</h6>
+    <button @click="changeToy('狗狗')" >改变玩具</button>
+  </div>
+  </template>
+  <script setup lang='ts'>
+  import { ref, reactive } from 'vue';
+  import { useRouter } from 'vue-router';
+
+  defineProps(['toy', 'changeToy'])
+
+  
+  </script>
+  
+  <style scoped lang='scss'>
+  .child{
+  width: 100px;
+  height: 100px;
+  background-color: pink;
+}
+  </style>
---
+++ src/views/props/Father.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="father">
+  我是父元素
+  <Child :toy="toy"  :changeToy="changeToy"/>
+</div>
+</template>
+<script setup lang='ts'>
+import { ref, reactive } from 'vue';
+import { useRouter } from 'vue-router';
+
+import Child from './Child.vue';
+
+
+let toy = ref('变形金刚')
+
+let changeToy = (value:string) => {
+  toy.value = value || '奥特曼'
+}
+
+</script>
+
+<style scoped lang='scss'>
+.father{
+  width: 200px;
+  height: 200px;
+  background-color: lightblue;
+  color: #333;
+}
+</style>
